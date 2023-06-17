24899888c32026d31fe650ac96319aa4c47eb1e8
itt828 <iittt828@gmail.com>
2023-06-17 10:48:38 +0900

    :construction: Impl base

---
+++ src/components/Grass.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <NCard>
+      <NLayout :native-scrollbar="false" bordered>
+        <div :class="$style.container">
+          <div v-for="i in days">
+            <div :class="$style.grass" />
+          </div>
+        </div>
+      </NLayout>
+    </NCard>
+  </div>
+</template>
+<script setup lang="ts">
+import { NCard, NLayout } from 'naive-ui'
+import { ref } from 'vue'
+
+const days = ref(Array(183).fill(0))
+</script>
+
+<style module lang="scss">
+.container {
+  width: 100%;
+  height: 100%;
+  gap: 6px;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-rows: repeat(5, 1fr);
+}
+.grass {
+  height: 12px;
+  width: 12px;
+  background-color: green;
+}
+</style>
---
+++ src/views/PersonalScore.vue
@@ -0,0 +1,12 @@
+<template>
+  <NSpace vertical>
+    <Grass />
+    <DispayItemSelector />
+  </NSpace>
+</template>
+<script setup lang="ts">
+import Grass from '@/components/Grass.vue'
+import DispayItemSelector from '@/components/DisplayItemSelector.vue'
+import { NSpace } from 'naive-ui'
+</script>
+<style module lang="scss"></style>
