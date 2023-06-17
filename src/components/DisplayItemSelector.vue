43e13b3a929dd22e766b58c0bafcaae3a1b1ad55
itt828 <iittt828@gmail.com>
2023-06-17 15:47:16 +0900

    :zap: Use store

--- src/components/DisplayItemSelector.vue
+++ src/components/DisplayItemSelector.vue
@@ -1,27 +1,42 @@
 <template>
   <NCard>
     <NLayout>
-      <NLayoutContent>
-        <NRadio>hoge</NRadio>
-      </NLayoutContent>
-      <NLayoutContent>
+      <NLayoutContent v-for="item in items" :key="item.id">
         <NRadio
-          >hogeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</NRadio
-        >
-      </NLayoutContent>
-      <NLayoutContent>
-        <NRadio
-          >hogeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</NRadio
-        >
-      </NLayoutContent>
-      <NLayoutContent>
-        <NRadio
-          >hogeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</NRadio
-        >
+          name="display-item"
+          :value="item.id"
+          :checked="selectedItem === item.id"
+          @change="handleChange"
+          >{{ item.title }}
+        </NRadio>
       </NLayoutContent>
     </NLayout>
   </NCard>
 </template>
 <script setup lang="ts">
 import { NLayout, NLayoutContent, NRadio, NCard } from 'naive-ui'
+import { onMounted, ref } from 'vue'
+import { useItemStore } from '@/stores/item'
+import { storeToRefs } from 'pinia'
+
+const props = defineProps<{}>()
+const itemStore = useItemStore()
+const { items } = storeToRefs(itemStore)
+const selectedItem = ref<string | null>(null)
+const gen = () => {
+  return { id: String(Math.random()), title: 'hoge', score: 20 }
+}
+onMounted(async () => {
+  await itemStore.fetchItems()
+  items.value.push(gen())
+  items.value.push(gen())
+  items.value.push(gen())
+  items.value.push(gen())
+  items.value.push(gen())
+  items.value.push(gen())
+})
+
+const handleChange = (e: Event) => {
+  selectedItem.value = (e.target as HTMLInputElement).value
+}
 </script>
